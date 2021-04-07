93b52c1be172ddf89e6e8088952e237fd476ace3
Karina <karinaalveslima@icloud.com>
2021-04-06 22:40:29 -0300

    programa de estoque

---
+++ classes/Item.class.php
@@ -0,0 +1,11 @@
+<?php
+
+require (__DIR__ . '/../interfaces/item.interface.php');
+
+class Item implements iTem{
+    public function setDados(array $dados):bool{
+
+    }
+    public function getDados(int $id_item):array{
+    }
+}
